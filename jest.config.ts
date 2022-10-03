ae400c5756e8eb712aa9f10800edba6eed2bf85e
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-10-03 18:11:36 -0300

    chore: jest

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  maxWorkers: '50%',
+  testEnvironment: 'jest-environment-node',
+  testPathIgnorePatterns: ['/node_modules/'],
+  roots: ['<rootDir>/src']
+}

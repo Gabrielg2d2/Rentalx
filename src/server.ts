13b368291e051945c4110f69df73217c31edc90e
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-11-08 18:23:21 -0300

    feat: 🎸 container docker postgres

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
-import swaggerFile from './swaggerFile.json'
+
 import { router } from './routes'
+import swaggerFile from './swaggerFile.json'
+
+import './database'
 
 const app = express()
 

9c57af00291e55734436db2a8e48ef99fd3e3df2
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-11-08 19:14:44 -0300

    refactor: 💡 docker-compose

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,15 @@
-import { createConnection } from 'typeorm'
+/* eslint-disable @typescript-eslint/no-floating-promises */
 
-void createConnection()
+import { createConnection, getConnectionOptions } from 'typeorm'
+
+interface IOptions {
+  host: string
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions
+  newOptions.host = 'database' // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options
+  })
+})
